b35ebebfd23c5e3feecde3b3d0f885ff7d92e128
redfish-github <127568412+redfish-github@users.noreply.github.com>
2023-03-12 14:15:15 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -8,12 +8,11 @@ function check()
             	location.replace("https://redfish-github.github.io/html/时间.html");
 	    	}
 	    	if(x==""){
-				slert("输入为空")
-			if(x=="图片"){
-				location.replace("https://redfish-github.github.io/html/tp.html");
-			}
-			if(x=="资源"){
-				location.replace("https://redfish-github.github.io/html/zy.html");
-			}
+			slert("输入为空")
+		if(x=="图片"){
+			location.replace("https://redfish-github.github.io/html/tp.html");
 		}
-    }
+		if(x=="资源"){
+			location.replace("https://redfish-github.github.io/html/zy.html");
+		}
+	}
