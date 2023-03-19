0026a993ed85bb4bd517fcbaac9b1bc93abb6f23
redfish-github <red_fish_mail@163.com>
2023-03-19 15:53:02 +0800

    jm++

--- other/js/jm.js
+++ other/js/jm.js
@@ -1,13 +1,13 @@
 
 function as(){
     var v= document.getElementById("ulo").value;
-    var v_1 =escape(v);//编码
+    var v_1 = window.btoa(v); // 加密
     var l=document.getElementById("rulo");
     l.value=v_1;
 };
 function sa(){
     var y= document.getElementById("lo").value;
-    var y_1 = unescape(y); //解码
+    var y_1 = window.atob(y); // 解密
     var u=document.getElementById("relo");
     u.value=y_1;
 };
